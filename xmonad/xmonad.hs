bcefbe63a32a09d2f230474dd334546bdc338708
Cedric Girard <girard.cedric@gmail.com>
2013-09-09 12:14:49 +0200

    change the way yeganesh is called

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -78,7 +78,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
 
     -- launch dmenu
-    , ((modm,               xK_p     ), spawn "exe=`dmenu_path_c | yeganesh` && eval \"exec $exe\"")
+    , ((modm,               xK_p     ), spawn "exe=`yeganesh -x` && eval \"exec $exe\"")
 
     -- launch gmrun
     , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
